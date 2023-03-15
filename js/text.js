2735facae9f6f5cad67c72f36720728ecdf0bfb4
Abhichoudhary <abhichoudhary10112001@gmail.com>
2023-03-15 15:09:00 +0530

    first commit

---
+++ js/text.js
@@ -0,0 +1,8 @@
+window.addEventListener('scroll', reveal);
+function reveal(){
+    var reveal = document.querySelectorAll('.reveal');
+    for(var i=0; i<reveal.length; i++){
+        var widndowheight = window.innerHeight;
+        var revealtop = reveals
+    }
+}
